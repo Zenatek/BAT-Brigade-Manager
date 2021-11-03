d551b870ac0f7e10c6bceb5c20a6c584feccc5f4
Rootx86 <filippo.lapide@gmail.com>
2021-11-03 13:19:59 +0100

    check message on invite leaderboard channel

---
+++ bot.py
@@ -0,0 +1,33 @@
+from discord import channel
+import requests
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+import time
+import os
+from datetime import datetime
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='YOU!'))
+
+@client.event
+async def on_message(message):
+    user = message.author
+    # Delete message if they aren't of Invite tracker bot
+    if user.id != 720351927581278219:
+        await message.delete()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(dt_string +" "+ str(user) + " " + str(message.content))
+
+
+if __name__ == '__main__':
+    print("Running")
+    client.run(os.environ.get('BATMANAGER'))
