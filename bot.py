494cd439e7e83d0e19ff7f2ee9c1de0c09373be4
Zenatek <filippo.lapide@gmail.com>
2021-11-15 15:00:52 +0100

    add leaderboard check messages

--- bot.py
+++ bot.py
@@ -17,15 +17,25 @@ client = discord.Client(intents=intents)
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='YOU!'))
 
-@client.event
 async def on_message(message):
     user = message.author
-    # Delete message if they aren't of Invite tracker bot
-    if user.id != 720351927581278219:
+    ch = message.channel.id
+    if ch == 905246431298875453:
+      # Delete message if they aren't of Invite tracker bot
+      if user.id != 720351927581278219:
         await message.delete()
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print(dt_string +" "+ str(user) + " " + str(message.content))
+    elif ch == 903398764885200916:
+      # Delete message if they aren't of MEE6 bot
+      if user.id != 159985870458322944:
+        await message.delete()
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(dt_string +" "+ str(user) + " " + str(message.content))
+
+
 
 
 if __name__ == '__main__':
